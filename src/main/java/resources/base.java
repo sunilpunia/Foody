f8baa852dbdb5c2fa4aca2923fcc14c707127cf7
sunil.punia <sunil.punia@BLUEAVES-3>
2021-07-27 19:55:05 +0530

    2nd commit

--- src/main/java/resources/base.java
+++ src/main/java/resources/base.java
@@ -66,7 +66,7 @@ public class base {
 	
 	}
 	
-	
+	//test
 	public void getScreenshot(String result) throws IOException {
 		File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
 		FileUtils.copyFile(src, new File("C://test//" + result + "screenshot.png"));
